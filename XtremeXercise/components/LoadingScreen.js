2dce933fb13021620f0d68866f1fbace0e4800f1
jmenke <jacqueline-menke@gmx.de>
2018-06-20 00:44:07 +0200

    Ladebildschirm angepasst, Layout, Spinner

--- XtremeXercise/components/LoadingScreen.js
+++ XtremeXercise/components/LoadingScreen.js
@@ -6,77 +6,125 @@ import {
   View,
   Button
 } from 'react-native';
+
+import {
+  BallIndicator,
+  BarIndicator,
+  DotIndicator,
+  MaterialIndicator,
+  PacmanIndicator,
+  PulseIndicator,
+  SkypeIndicator,
+  UIActivityIndicator,
+  WaveIndicator,
+} from 'react-native-indicators';
+
 export default class LoadingScreen extends Component {
     
     render(){
       const { navigate}=this.props.navigation;
       return(
+
         <View style={styles.container}>
-          <Text style={[styles.accentColor1, styles.huge]}>
-            X
-            <Text style={[styles.textColor, styles.big]}>
-              treme
-            </Text>
-          </Text>
-          <Text style={[styles.accentColor1, styles.huge]}>
-            X
-            <Text style={[styles.textColor, styles.big]}>
-              ercising
-            </Text>
-          </Text>
+
+          <View style={styles.header}> 
+            <View style={styles.headerElem1}>
+              <Text style={styles.headerText1}>
+                X
+                <Text style={styles.headerText2}>
+                  treme
+                </Text>
+              </Text>
+            </View>
+
+            <View style={styles.headerElem2}>
+              <Text style={styles.headerText1}>
+                X
+                <Text style={styles.headerText2}>
+                  ercising
+                </Text>
+              </Text>
+            </View>
+          </View>
           
-          <Button
-            title ="Navigate to Home"
-            onPress = { ()=> navigate('Home')}
-            color = '#EF2E1C'/>
-            <Text style={styles.normal}>Be the best version of yourself</Text>
+          <View style={styles.loadingImage2}>
+            <Button
+              title ="Navigate to Home"
+              onPress = { ()=> navigate('Home')}
+              color = '#EF2E1C'/>
+              <BallIndicator color = '#EF6A39' size = {50}/>
+          </View>
+
+          <View style={styles.quote}>
+            <Text style={styles.quoteText1}>
+                Be the best version of 
+                <Text style={styles.quoteText2}>
+                  h
+                </Text>
+                <Text style={styles.quoteText3}>
+                  yourself
+                </Text>
+              </Text>
+          </View>
+
         </View>
       );
     }
   }
+
+
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
       backgroundColor: '#372D29',
+      flex:1,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
     },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
+    header: {
+      flex: 3,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
     },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
+    headerElem1: {
+      marginLeft: -80,
     },
-    boldStyle: {
-      fontWeight: 'bold',
+    headerElem2: {
+      marginLeft: 40,
     },
-    accentColor1: {
+    headerText1: {
       color: '#EF2E1C',
+      fontSize: 55,
     },
-    accentColor2: {
-      color: '#EF6A39',
-    },
-    primaryColor1: {
-      color: '#372D29',
-    },
-    primaryColor2: {
-      color: '#564640',
-    },
-    textColor: {
+    headerText2: {
       color: '#FFFFFF',
-    },
-    huge: {
       fontSize: 40,
     },
-    big: {
-      fontSize: 30,
+    loadingImage2: {
+      flex: 2,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
     },
-    normal: {
-      fontSize: 15,
+    quote: {
+      flex: 1,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    quoteText1: {
+      color: '#FFFFFF',
+      fontSize: 17,
+    },
+    quoteText2: {
+      color: '#372D29',
+      fontSize: 10,
+    },
+    quoteText3: {
+      color: '#EF2E1C',
+      fontSize: 19,
+      fontWeight: 'bold',
     },
   });
 
