85a016a6d8ee06597fc37aaeec091902064e10f6
jmenke <jacqueline-menke@gmx.de>
2018-06-20 16:10:34 +0200

    Meine Trainingspläne angepasst

--- XtremeXercise/components/HomeScreen.js
+++ XtremeXercise/components/HomeScreen.js
@@ -6,37 +6,78 @@ import {
   View,
   Button
 } from 'react-native';
+
 export default class HomeScreen extends Component {
     render(){
       const { navigate}=this.props.navigation;
       return(
         <View style={styles.container}>
-          <Button
-            title ="Mein Trainingsplan"
-            onPress = { ()=> navigate('Trainingsplan')}/>
-        <Button
-            title ="Statistik"
-            onPress = { ()=> navigate('Statistik')}/>
+          <View style={styles.header}>
+            <Text style={styles.headerText1}> 
+              |
+              <Text style={styles.headerText2}>
+                Meine Trainingspläne
+              </Text>
+            </Text>
+          </View>
+
+          <View style={styles.list}>
+
+          </View>
+          
+          <View style={styles.buttons}>
+            <Button
+              title ="Mein Trainingsplan"
+              onPress = { ()=> navigate('Trainingsplan')}/>
+            <Button
+              title ="Statistik"
+              onPress = { ()=> navigate('Statistik')}/>
+          </View>
+
         </View>
       );
     }
   }
+
   const styles = StyleSheet.create({
     container: {
+      backgroundColor: '#372D29',
+      flex:1,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
+    },  
+    header: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F500FF',
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
     },
-    welcome: {
+    headerElem1: {
+      marginLeft: -80,
+    },
+    headerElem2: {
+      marginLeft: 40,
+    },
+    headerText1: {
+      color: '#EF2E1C',
       fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
     },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
+    headerText2: {
+      color: '#FFFFFF',
+      fontSize: 20,
+    },
+    list: {
+      flex: 2,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    buttons: {
+      flex: 1,
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center',
     },
   });
   
