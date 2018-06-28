071a378a9dd53a78976f2925ecc1b6692e723f4b
finitoxx <xxfinitoxx@gmx.de>
2018-06-28 02:46:26 +0200

    Liste in Traininieren Screen hinzugefügt

---
+++ XtremeXercise/components/TS_components/ÜbungseinheitenListComponent.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { View, FlatList , StyleSheet, TouchableOpacity } from "react-native";
+import ListElement from "./ListElementComponent";
+import * as allData from './../../Daten.json';
+
+export default class ÜbungseinheitenListComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: allData.Übungseinheiten,
+    };
+  }
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#000000",
+        }}
+      />
+    );
+  };
+  render() {
+    const { navigate}=this.props.navigation;
+    return (
+      <View style={styles.list}>
+        
+            <FlatList
+                data={this.state.data}
+                renderItem={({item})=>(
+                    <TouchableOpacity onPress = { ()=> navigate('ÜbungAusführen',{trainingsplan: item})}>
+                    <ListElement 
+                    uebungseinheit= {item}
+                    />
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item, name) => item.name}
+                ItemSeparatorComponent={this.renderSeparator}
+            />
+        
+      </View>
+      
+    );
+   
+  }
+  
+}
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        width: "100%",
+        borderTopWidth: 2, 
+        borderBottomWidth: 2,
+        borderLeftWidth: 2,
+    },
+}) 
