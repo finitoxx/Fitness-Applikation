29ea5f4d6f80f7ffbc1814e6b7dc8edaf101e348
finitoxx <psquarra1@fh-bielefeld.de>
2018-07-05 11:08:33 +0200

    Übungsdaten angelegt und Listmethoden erstellt

--- XtremeXercise/components/ÜS_components/ÜbungenScreen.js
+++ XtremeXercise/components/ÜS_components/ÜbungenScreen.js
@@ -9,52 +9,83 @@ import {
 } from 'react-native';
 import UebungHinzufügen from './ÜbungHinzufügen';
 import Modal from "react-native-modal";
+import * as allData from "./../../Daten.json"
 
 export default class ÜbungenScreen extends Component {
 
   state = {
     isModalVisible: false,
     selection: "Alphabetisch",
-    data:[{
-      "name": "Squat",
-      "muskelgruppe":["Po","Oberschenke"],
-      "kategorie" : "Freihantel"
-    }],
-    list:[
-      { title: 'A', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'A')},
-      { title: 'B', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'B')},
-      { title: 'C', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'C')},
-      { title: 'D', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'D')},
-      { title: 'E', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'E')},
-      { title: 'F', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'F')},
-      { title: 'G', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'G')},
-      { title: 'H', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'H')},
-      { title: 'I', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'I')},
-      { title: 'J', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'J')},
-      { title: 'K', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'K')},
-      { title: 'L', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'L')},
-      { title: 'M', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'M')},
-      { title: 'N', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'N')},
-      { title: 'O', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'O')},
-      { title: 'P', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'P')},
-      { title: 'Q', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'Q')},
-      { title: 'R', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'R')},
-      { title: 'S', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'S')},
-      { title: 'T', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'T')},
-      { title: 'U', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'U')},
-      { title: 'V', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'V')},
-      { title: 'W', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'W')},
-      { title: 'X', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'X')},
-      { title: 'Y', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'Y')},
-      { title: 'Z', data: this.state.data.filter((übung) => übung.name.toUppercase().charAt(0) = 'Z')},
-    ],
+    data:allData.Übung,
+    list: [],
   };
 
   _toggleModal = () =>
     this.setState({ isModalVisible: !this.state.isModalVisible });
 
   _addÜbungseinheit = () =>{}
-
+  
+  componentDidMount = () =>{
+    this.setState({
+      list: this._alphabetListe()
+    })
+  }
+  _alphabetListe = () =>{
+    return(
+      [
+        { title: 'A', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'A')},
+        { title: 'B', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'B')},
+        { title: 'C', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'C')},
+        { title: 'D', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'D')},
+        { title: 'E', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'E')},
+        { title: 'F', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'F')},
+        { title: 'G', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'G')},
+        { title: 'H', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'H')},
+        { title: 'I', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'I')},
+        { title: 'J', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'J')},
+        { title: 'K', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'K')},
+        { title: 'L', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'L')},
+        { title: 'M', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'M')},
+        { title: 'N', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'N')},
+        { title: 'O', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'O')},
+        { title: 'P', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'P')},
+        { title: 'Q', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'Q')},
+        { title: 'R', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'R')},
+        { title: 'S', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'S')},
+        { title: 'T', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'T')},
+        { title: 'U', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'U')},
+        { title: 'V', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'V')},
+        { title: 'W', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'W')},
+        { title: 'X', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'X')},
+        { title: 'Y', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'Y')},
+        { title: 'Z', data: this.state.data.filter((übung) => übung.name.toUpperCase().charAt(0) == 'Z')},
+      ]
+    )
+  }
+  _kategorieListe = () =>{
+    return(
+      [
+        {title: 'Aufwärmen', data: this.state.data.filter((übung) => übung.kategorie = 'Aufwärmen')},
+        {title: 'Freihantel', data: this.state.data.filter((übung) => übung.kategorie = 'Freihantel')},
+        {title: 'Maschine', data: this.state.data.filter((übung) => übung.kategorie = 'Maschine')},
+      ]
+    );
+  }
+  _muskelgruppeListe = () =>{
+    return(
+      [
+        {title: 'Bauch', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Bauch')},
+        {title: 'Bizeps', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Bizeps')},
+        {title: 'Brust', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Brust')},
+        {title: 'Oberschenkel', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Oberschenkel')},
+        {title: 'Unterschenkel', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Unterschenkel')},
+        {title: 'Waden', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Waden')},
+        {title: 'Trizeps', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Trizeps')},
+        {title: 'Rücken', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Rücken')},
+        {title: 'Po', data: this.state.data.filter((übung) => übung.muskelgruppe.include = 'Po')},
+      ]
+    );
+  }
     render(){
       const { navigate}=this.props.navigation;
       return(
@@ -90,22 +121,35 @@ export default class ÜbungenScreen extends Component {
             mode='dropdown'
             selectedValue={this.state.selection}
             style={{ height: 50 , width: 200}}
-            onValueChange={(itemValue, itemIndex) => this.setState({selection: itemValue})}>
+            onValueChange={(itemValue, itemIndex) => {
+              switch(itemValue){
+                case "Alphabetisch":
+                  this.setState({list: this._alphabetListe})
+                  break;
+                case "Kategorie":
+                  this.setState({list: this._kategorieListe})
+                  break;
+                case "Muskelgruppen":
+                  this.setState({list: this._muskelgruppeListe})
+                  break;
+              }
+              this.setState({selection: itemValue})}
+              }>
             <Picker.Item label="Alphabetisch" value="Alphabetisch" />
             <Picker.Item label="Kategorie" value="Kategorie" />
             <Picker.Item label="Muskelgruppen" value="Muskelgruppen" />
           </Picker>
          </View>
-        <View style ={styles.list}>
-          <SectionList 
-          sections={}
-          renderSectionHeader={ ({section}) => <Text style={styles.SectionHeaderStyle}> { section.title } </Text> }
-          renderItem={ ({item}) => <Text style={styles.SectionListItemStyle} onPress={this.GetSectionListItem.bind(this, item)}> { item } </Text> }
-          keyExtractor={ (item, index) => index }
-          />
+        
         </View>
-        </View>
-
+          <View style ={styles.list}>
+            <SectionList 
+            sections={this.state.list}
+            renderSectionHeader={ ({section}) => <Text style={styles.SectionHeaderStyle}> { section.title } </Text> }
+            renderItem={ ({item}) => <Text style={styles.SectionListItemStyle}></Text> }
+            keyExtractor={ (item, index) => index }
+            />
+          </View>
           <Button
             title ="B2B"
             onPress = { ()=> navigate('EditTrainingsplan')}/>
@@ -150,7 +194,7 @@ export default class ÜbungenScreen extends Component {
       alignItems: 'flex-end',
     },
     list: {
-      flex: 1,
+      flex: 9,
     },
     SectionHeaderStyle:{
  
